fea99d74feeaa8c4e37e06dee97bcc36941a498e
Francy Rondon <cranfhuo@gmail.com>
2024-04-12 17:56:24 -0400

    2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
+import BestCharacters from "../components/BestCharacters/BestCharacters";
+
 export default function Home() {
     return(
         <div>
-           
+           <BestCharacters />
         </div>
     )
 };
